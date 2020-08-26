c87c89c3b9a279b896fa7d43d9b0b3e51f371e41
Olga <olgaaevdokim@gmail.com>
2020-08-26 15:52:37 +0300

    Edit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -59,7 +59,7 @@ public class Main {
             int i = sc.nextInt();
             System.out.println("Input a column");
             int j = sc.nextInt();
-            if (!filling(arr, i , j)) { // если клетка без начинки(пустая)
+            if (!filling(arr, i, j)) { // если клетка без начинки(пустая)
                 arr[i][j] = 1;
                 break;
             }
@@ -69,15 +69,15 @@ public class Main {
     static boolean filling(int[][] arr, int i, int j) {
         for (int n = i; n < arr.length; n++) {
             for (int k = j; k < arr.length; k++) {
-                if ( arr[n][k] == 2) {
-                   return false;
+                if (arr[n][k] == 2) {
+                    return false;
                 } else if (arr[n][k] == 1 || arr[n][k] == 0) {
                     System.out.println("Клетка занята введите другую");
                     return true;
                 }
             }
         }
-       return false;
+        return false;
     }
 
     static void checkComp(int[][] arr) {  //ввод компа (0)
@@ -91,65 +91,12 @@ public class Main {
         if (quantity >= 2) {
 
             //  ищем  два 0, чтобы поставить третий ноль и выиграть
-            if (checkZero(arr)) {
+            if (checkZeroOrOne(arr, 0)) {  // передаем ноль, потому что будем искать последовательность нолей
                 return;
             }
 
             // если , комп не выграл, ищем, куда поставить 0, чтобы перекрыть единицы и не дать человеку выиграть
-            //проверка горизонтали
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i][0] == 1 && arr[i][1] == 1 && arr[i][2] == 2) {
-                    stepComp(arr, i, 2);
-                    return;
-                }
-                if (arr[i][0] == 1 && arr[i][1] == 2 && arr[i][2] == 1) {
-                    stepComp(arr, i, 1);
-                    return;
-                }
-                if (arr[i][0] == 2 && arr[i][1] == 1 && arr[i][2] == 1) {
-                    stepComp(arr, i, 0);
-                    return;
-                }
-            }
-            //проверка 1 диагонали
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[0][i] == 1 && arr[1][i] == 1 && arr[2][i] == 2) {
-                    stepComp(arr, 2, i);
-                    return;
-                }
-                if (arr[0][i] == 1 && arr[1][i] == 2 && arr[2][i] == 1) {
-                    stepComp(arr, 1, i);
-                    return;
-                }
-                if (arr[0][i] == 2 && arr[1][i] == 1 && arr[2][i] == 1) {
-                    stepComp(arr, 0, i);
-                    return;
-                }
-            }
-            //проверка  1 диагонали
-            if (arr[0][0] == 1 && arr[1][1] == 1 && arr[2][2] == 2) {
-                stepComp(arr, 2, 2);
-                return;
-            }
-            if (arr[0][0] == 1 && arr[1][1] == 2 && arr[2][2] == 1) {
-                stepComp(arr, 1, 1);
-                return;
-            }
-            if (arr[0][0] == 2 && arr[1][1] == 1 && arr[2][2] == 1) {
-                stepComp(arr, 0, 0);
-                return;
-            }
-            //проверка  2 горизонтали
-            if (arr[2][0] == 1 && arr[1][1] == 1 && arr[0][2] == 2) {
-                stepComp(arr, 0, 2);
-                return;
-            }
-            if (arr[2][0] == 1 && arr[1][1] == 2 && arr[0][2] == 1) {
-                stepComp(arr, 1, 1);
-                return;
-            }
-            if (arr[2][0] == 2 && arr[1][1] == 1 && arr[0][2] == 1) {
-                stepComp(arr, 2, 0);
+            if (checkZeroOrOne(arr, 1)) {  // передаем единицу, потому что будем искать последовательность единиц
                 return;
             }
 
@@ -179,61 +126,62 @@ public class Main {
 
     }
 
-    static boolean checkZero(int[][] arr) { // ищем, есть ли на поле последовательность 0, чтобы поставить тертий ноль и выиграть
+    static boolean checkZeroOrOne(int[][] arr, int zeroOrOne) { // ищем, есть ли на поле последовательность 0, чтобы поставить тертий ноль и выиграть
         //проверка горизонтали
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i][0] == 0 && arr[i][1] == 0 && arr[i][2] == 2) {
+            if (arr[i][0] == zeroOrOne && arr[i][1] == zeroOrOne && arr[i][2] == 2) {
                 stepComp(arr, i, 2);
                 return true;
             }
-            if (arr[i][0] == 0 && arr[i][1] == 2 && arr[i][2] == 0) {
+            if (arr[i][0] == zeroOrOne && arr[i][1] == 2 && arr[i][2] == zeroOrOne) {
                 stepComp(arr, i, 1);
                 return true;
             }
-            if (arr[i][0] == 2 && arr[i][1] == 0 && arr[i][2] == 0) {
+            if (arr[i][0] == 2 && arr[i][1] == zeroOrOne && arr[i][2] == zeroOrOne) {
                 stepComp(arr, i, 0);
                 return true;
             }
         }
         //проверка 1 диагонали
         for (int i = 0; i < arr.length; i++) {
-            if (arr[0][i] == 0 && arr[1][i] == 0 && arr[2][i] == 2) {
+            if (arr[0][i] == zeroOrOne && arr[1][i] == zeroOrOne && arr[2][i] == 2) {
                 stepComp(arr, 2, i);
                 return true;
             }
-            if (arr[0][i] == 0 && arr[1][i] == 2 && arr[2][i] == 0) {
+            if (arr[0][i] == zeroOrOne && arr[1][i] == 2 && arr[2][i] == zeroOrOne) {
                 stepComp(arr, 1, i);
                 return true;
             }
-            if (arr[0][i] == 2 && arr[1][i] == 0 && arr[2][i] == 0) {
+            if (arr[0][i] == 2 && arr[1][i] == zeroOrOne && arr[2][i] == zeroOrOne) {
                 stepComp(arr, 0, i);
                 return true;
             }
         }
         //проверка  1 диагонали
-        if (arr[0][0] == 0 && arr[1][1] == 0 && arr[2][2] == 2) {
+        if (arr[0][0] == zeroOrOne && arr[1][1] == zeroOrOne && arr[2][2] == 2) {
             stepComp(arr, 2, 2);
             return true;
         }
-        if (arr[0][0] == 0 && arr[1][1] == 2 && arr[2][2] == 0) {
+        if (arr[0][0] == zeroOrOne && arr[1][1] == 2 && arr[2][2] == zeroOrOne) {
             stepComp(arr, 1, 1);
             return true;
         }
-        if (arr[0][0] == 2 && arr[1][1] == 0 && arr[2][2] == 0) {
+        if (arr[0][0] == 2 && arr[1][1] == zeroOrOne && arr[2][2] == zeroOrOne) {
             stepComp(arr, 0, 0);
             return true;
         }
         //проверка  2 горизонтали
-        if (arr[2][0] == 0 && arr[1][1] == 0 && arr[0][2] == 2) {
+        if (arr[2][0] == zeroOrOne && arr[1][1] == zeroOrOne && arr[0][2] == 2) {
             stepComp(arr, 0, 2);
             return true;
         }
-        if (arr[2][0] == 0 && arr[1][1] == 2 && arr[0][2] == 0) {
+        if (arr[2][0] == zeroOrOne && arr[1][1] == 2 && arr[0][2] == zeroOrOne) {
             stepComp(arr, 1, 1);
             return true;
         }
-        if (arr[2][0] == 2 && arr[1][1] == 0 && arr[0][2] == 0) {
+        if (arr[2][0] == 2 && arr[1][1] == zeroOrOne && arr[0][2] == zeroOrOne) {
             stepComp(arr, 2, 0);
+            return true;
         }
 
         return false;
@@ -292,14 +240,10 @@ public class Main {
                 System.out.println("Win");
                 return true;
             }
-            if (arr[2][0] == 1 && arr[1][1] == 1 && arr[0][2] == 1) {
-                System.out.println("Win");
-                return true;
-            }
-            if (arr[2][2] == 1 && arr[1][1] == 1 && arr[0][0] == 1) {
-                System.out.println("Win");
-                return true;
-            }
+           if (checkDiag(arr, 1)){
+               return true;
+           }
+
             //проверка на проигрыш
             if (arr[i][0] == 0 && arr[i][1] == 0 && arr[i][2] == 0) {
                 System.out.println("Failed");
@@ -309,12 +253,7 @@ public class Main {
                 System.out.println("Failed");
                 return true;
             }
-            if (arr[0][0] == 0 && arr[1][1] == 0 && arr[2][2] == 0) {
-                System.out.println("Failed");
-                return true;
-            }
-            if (arr[2][0] == 0 && arr[1][1] == 0 && arr[0][2] == 0) {
-                System.out.println("Failed");
+            if (checkDiag(arr, 0)){
                 return true;
             }
 
@@ -326,6 +265,29 @@ public class Main {
             return true;
         }
 
+        return false;
+    }
+
+    static boolean checkDiag(int[][] arr, int zeroOrOne) {
+        String win = "Win";
+        String failed = "Failed";
+        if (arr[0][0] == zeroOrOne && arr[1][1] == zeroOrOne && arr[2][2] == zeroOrOne) {
+            if (zeroOrOne == 1) {
+                System.out.println(win);
+            } else {
+                System.out.println(failed);
+            }
+            return true;
+        }
+
+        if (arr[2][0] == zeroOrOne && arr[1][1] == zeroOrOne && arr[0][2] == zeroOrOne) {
+            if (zeroOrOne == 1) {
+                System.out.println(win);
+            } else {
+                System.out.println(failed);
+            }
+            return true;
+        }
         return false;
     }
 
